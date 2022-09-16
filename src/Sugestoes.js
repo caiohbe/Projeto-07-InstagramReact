df5d978d01de563c8e759b4f78487bf8af7c766a
caiohbe <caiohbe@gmail.com>
2022-09-16 17:51:24 -0300

    SideBar fix

--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -1,3 +1,5 @@
 export default function Sugestoes() {
-    return
+    return (
+        <div></div>
+    )
 }
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,3 +1,7 @@
 export default function Usuario() {
-    return
+    return (
+        <div>
+            
+        </div>
+    )
 }
