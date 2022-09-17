a773dd56d5e2e0727ce15b5ed522bfbd22691836
caiohbe <caiohbe@gmail.com>
2022-09-16 22:18:14 -0300

    User fix

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,17 @@
+import React from "react"
+
 export default function Usuario() {
+    const [name, setName] = React.useState('Catana')
+    const [img, setImg] = React.useState('assets/img/catanacomics.svg')
+
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
+            <img src={img} onClick={() => setImg((prompt('Digite a URL da sua imagem:')))}/>
             <div class="texto">
                 <strong>catanacomics</strong>
                 <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                    {name}
+                    <ion-icon name="pencil" onClick={() => setName((prompt('Digite seu nome:')))}></ion-icon>
                 </span>
             </div>
         </div>
