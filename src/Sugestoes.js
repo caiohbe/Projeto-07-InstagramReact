e5a838b3a5e69c2d1ebfb43515652dd687331c1a
caiohbe <caiohbe@gmail.com>
2022-09-16 22:06:01 -0300

    Sugestions

--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -1,5 +1,34 @@
+const sugestions = [
+    { user: 'bad.vibes.memes', img: 'assets/img/bad.vibes.memes.svg', desc: 'Segue você' },
+    { user: 'chibirdart', img: 'assets/img/chibirdart.svg', desc: 'Segue você' },
+    { user: 'razoesparaacreditar', img: 'assets/img/razoesparaacreditar.svg', desc: 'Novo no Instagram' },
+    { user: 'adorable_animals', img: 'assets/img/adorable_animals.svg', desc: 'Segue você' },
+    { user: 'smallcutecats', img: 'assets/img/smallcutecats.svg', desc: 'Segue você' },
+]
+
+const sugestionDiv = sugestions.map((s) => {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={s.img} />
+                <div class="texto">
+                    <div class="nome">{s.user}</div>
+                    <div class="razao">{s.desc}</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+})
 export default function Sugestoes() {
     return (
-        <div></div>
+        <div class="sugestoes">
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+            {sugestionDiv}
+        </div>
     )
 }
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,7 +1,14 @@
 export default function Usuario() {
     return (
-        <div>
-            
+        <div class="usuario">
+            <img src="assets/img/catanacomics.svg" />
+            <div class="texto">
+                <strong>catanacomics</strong>
+                <span>
+                    Catana
+                    <ion-icon name="pencil"></ion-icon>
+                </span>
+            </div>
         </div>
     )
 }
