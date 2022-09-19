060ecbc829ce92e921a34bdfffc14d33d50f0ad9
caiohbe <caiohbe@gmail.com>
2022-09-19 17:04:18 -0300

    post/likes

--- src/Posts.js
+++ src/Posts.js
@@ -4,7 +4,6 @@ const postsContent = [
 ]
 
 export default function Posts() {
-
     const postDivs = postsContent.map((post) => {
         return (
             <div class="post">
@@ -19,18 +18,47 @@ export default function Posts() {
                 </div>
 
                 <div class="conteudo">
-                    <img src={post.img} />
+                    <img src={post.img} onClick={() => {
+                        document.querySelectorAll('.like')[postsContent.indexOf(post)].classList.add('escondido')
+                        document.querySelectorAll('.like')[postsContent.indexOf(post)].nextElementSibling.classList.remove('escondido')
+
+                        document.querySelectorAll('.texto')[postsContent.indexOf(post)].innerHTML = `
+                            Curtido por <strong>${post.likesTxt}</strong> e <strong>outras ${post.likesNum + 0.001} pessoas</strong>
+                        `
+                    }} />
                 </div>
 
                 <div class="fundo">
                     <div class="acoes">
                         <div>
-                            <ion-icon name="heart-outline" onClick={()=>alert('a')}></ion-icon>
+                            <ion-icon name="heart-outline" class="like" onClick={() => {
+                                document.querySelectorAll('.like')[postsContent.indexOf(post)].classList.add('escondido')
+                                document.querySelectorAll('.like')[postsContent.indexOf(post)].nextElementSibling.classList.remove('escondido')
+
+                                document.querySelectorAll('.texto')[postsContent.indexOf(post)].innerHTML = `
+                                    Curtido por <strong>${post.likesTxt}</strong> e <strong>outras ${post.likesNum + 0.001} pessoas</strong>
+                                 `
+                            }}>{post.likesNum}</ion-icon>
+                            <ion-icon name="heart" class="escondido" onClick={() => {
+                                document.querySelectorAll('.like')[postsContent.indexOf(post)].classList.remove('escondido')
+                                document.querySelectorAll('.like')[postsContent.indexOf(post)].nextElementSibling.classList.add('escondido')
+
+                                document.querySelectorAll('.texto')[postsContent.indexOf(post)].innerHTML = `
+                                    Curtido por <strong>${post.likesTxt}</strong> e <strong>outras ${post.likesNum} pessoas</strong>
+                                `
+                            }}></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
                         <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
+                            <ion-icon name="bookmark-outline" class="salvo" onClick={() => {
+                                document.querySelectorAll('.salvo')[postsContent.indexOf(post)].classList.add('escondido')
+                                document.querySelectorAll('.salvo')[postsContent.indexOf(post)].nextElementSibling.classList.remove('escondido')
+                            }}></ion-icon>
+                            <ion-icon name="bookmark" class="escondido" onClick={() => {
+                                document.querySelectorAll('.salvo')[postsContent.indexOf(post)].classList.remove('escondido')
+                                document.querySelectorAll('.salvo')[postsContent.indexOf(post)].nextElementSibling.classList.add('escondido')
+                            }}></ion-icon>
                         </div>
                     </div>
 
